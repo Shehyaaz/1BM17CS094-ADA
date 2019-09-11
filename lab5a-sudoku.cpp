8915ea7ee7dab88b078774aa64c6d524655e428a
Shehyaaz <49763208+Shehyaaz@users.noreply.github.com>
2019-09-11 13:10:45 +0530

    Update lab5a-sudoku.cpp

--- lab5a-sudoku.cpp
+++ lab5a-sudoku.cpp
@@ -1,12 +1,11 @@
 //this program solves sudoku puzzle
 
 #include <iostream>
+#include <cmath>
 #define UNASSIGNED 0
-#define N 9
-#define SQN 3
 using namespace std;
 
-bool FindUnassignedLocation(int grid[N][N], int &row, int &col){
+bool FindUnassignedLocation(int **grid, int &row, int &col, int N){
 	for(row = 0; row<N; row++)
 		for(col = 0; col<N; col++)
 			if(grid[row][col] == UNASSIGNED)
@@ -14,21 +13,21 @@ bool FindUnassignedLocation(int grid[N][N], int &row, int &col){
 	return false;
 }
 
-bool UsedInRow(int grid[N][N], int row, int num){
+bool UsedInRow(int **grid, int row, int num, int N){
 	for(int i=0; i<N; i++)
 		if(grid[row][i] == num)
 			return true;
 	return false;
 }
 
-bool UsedInCol(int grid[N][N], int col, int num){
+bool UsedInCol(int **grid, int col, int num, int N){
 	for(int i =0; i<N; i++)
 		if(grid[i][col] == num)
 			return true;
 	return false;
 }
 
-bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num){
+bool UsedInBox(int **grid, int boxStartRow, int boxStartCol, int num, int N, int SQN){
 	for(int i=0; i<SQN; i++)
 		for(int j=0; j<SQN;j++)
 			if(grid[i+boxStartRow][j+boxStartCol] == num)
@@ -36,18 +35,18 @@ bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num){
 	return false;
 }
 
-bool isSafe(int grid[N][N], int row, int col, int num){
-	return !UsedInRow(grid, row, num) && !UsedInCol(grid,col,num) && !UsedInBox(grid,row-row%SQN, col-col%SQN,num);
+bool isSafe(int **grid, int row, int col, int num, int N){
+	return !UsedInRow(grid, row, num, N) && !UsedInCol(grid,col,num, N) && !UsedInBox(grid,row-row%(int(sqrt(N))), col-col%(int(sqrt(N))),num, N, int(sqrt(N)));
 }
 
-bool solveSudoku(int grid[N][N]){
+bool solveSudoku(int **grid, int N){
 	int row, col;
-	if(!FindUnassignedLocation(grid,row, col))
+	if(!FindUnassignedLocation(grid,row, col,N))
 		return true;
 	for(int num = 1; num<=N; num++){
-		if(isSafe(grid,row,col,num)){
+		if(isSafe(grid,row,col,num,N)){
 			grid[row][col]=num;
-			if(solveSudoku(grid))
+			if(solveSudoku(grid,N))
 				return true;
 			grid[row][col] = UNASSIGNED;
 		}
@@ -56,12 +55,17 @@ bool solveSudoku(int grid[N][N]){
 }
 
 int main(){
-	int grid[N][N];
+	int **grid, N;
+	cout<<"Enter size :";
+	cin>>N;
+	grid = new int*[N];
+	for(int i =0;i<N;i++)
+		grid[i] = new int[N];
 	cout<<"Enter the Sudoku puzzle(0 for unfilled) :\n";
 	for(int i =0; i<N; i++)
 		for(int j=0;j<N;j++)
 			cin>>grid[i][j];
-	if(solveSudoku(grid)){
+	if(solveSudoku(grid,N)){
 		for(int i =0; i<N;i++){
 			for(int j=0;j<N;j++)
 				cout<<grid[i][j]<<"\t";
@@ -70,5 +74,8 @@ int main(){
 	}
 	return 0;
 }
+
+
+
 
 
